58544f644cbc8e04ca8c77d3dc4e8aebf85d8abc
funk2256 <funk2256@gmail.com>
2024-01-13 00:55:20 +0500

    laba6/Example8

--- src/laba6/Example7.java
+++ src/laba6/Example7.java
@@ -19,5 +19,5 @@ public class Example7 {
         }
         return intArray;
         }
-    
+
 }
--- src/laba6/Example8.java
+++ src/laba6/Example8.java
@@ -1,4 +1,27 @@
 package laba6;
 
+import java.util.Arrays;
+
 public class Example8 {
+    public static void main(String[] args) {
+        int[] arrayInt = new int[5];
+        arrayInt[0] = 10;
+        arrayInt[1] = 10;
+        arrayInt[2] = 10;
+        arrayInt[3] = 10;
+        arrayInt[4] = 10;
+
+        System.out.println("Получаем среднее значение: " + Arrays.toString(Example3.TestClass.getMiddleInt(arrayInt)));
+
+    }
+
+    public static int[] getMiddleInt(int[] arrayInt){
+        int sum;
+        sum = Arrays.stream(arrayInt).sum();
+        int [] midlearrays = new int [1];
+        midlearrays[0] = sum/arrayInt.length;
+
+
+        return midlearrays;
+    }
 }
