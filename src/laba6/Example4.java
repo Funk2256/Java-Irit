9b8927d1f51b366e4d489c8561d4ac3403294f8f
funk2256 <funk2256@gmail.com>
2024-01-13 00:34:40 +0500

    laba6/Example4

--- src/laba6/Example4.java
+++ src/laba6/Example4.java
@@ -1,4 +1,23 @@
 package laba6;
 
 public class Example4 {
+    public static void main(String[] args) {
+        int number = 5;
+        int doubleFactorial = DoubleFactorial.calculate(number);
+        System.out.println(number + "!! = " + doubleFactorial);
+
+    }
+
+    public class DoubleFactorial {
+        public static int calculate(int n) {
+            if (n <= 0) {
+                return 1;
+            }
+            int result = 1;
+            for (int i = n; i > 0; i -= 2) {
+                result *= i;
+            }
+            return result;
+        }
+    }
 }
