43cd1fa4aca6f054a1ac7347d46ef983458b774b
funk2256 <funk2256@gmail.com>
2024-01-13 00:50:34 +0500

    laba6/Example7

--- src/laba6/Example5.java
+++ src/laba6/Example5.java
@@ -1,4 +1,17 @@
 package laba6;
 
 public class Example5 {
+    public static void main(String[] args) {
+        int n = 7;
+        int sum = sumOfSquares(n);
+        System.out.println("Сумма квадратов натуральных чисел: " + sum);
+    }
+
+    public static int sumOfSquares(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i * i;
+        }
+        return sum;
+    }
 }
--- src/laba6/Example7.java
+++ src/laba6/Example7.java
@@ -1,4 +1,23 @@
 package laba6;
 
+import java.util.Arrays;
+
 public class Example7 {
+    public static void main(String[] args) {
+        //Создаем массив символов
+        char [] charsArray = {'a', 'b', 'c', 'd', 'e', 'f'};
+        int[] intArrays = charArtoCode(charsArray);
+
+        System.out.println(Arrays.toString(charsArray));
+        System.out.println(Arrays.toString(intArrays));
+    }
+
+    public static int[] charArtoCode(char[] charsArray) {
+        int[] intArray = new int[charsArray.length];
+        for (int i = 0; i < charsArray.length; i++) {
+            intArray[i] = (int) charsArray[i];
+        }
+        return intArray;
+        }
+    
 }
