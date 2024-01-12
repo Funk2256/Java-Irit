e12595cebbdde00eb393f7a693cb0cb3c3362b0f
funk2256 <funk2256@gmail.com>
2024-01-12 21:04:12 +0500

    Laba6Ex6

--- src/laba6/Example6.java
+++ src/laba6/Example6.java
@@ -1,4 +1,18 @@
 package laba6;
 
 public class Example6 {
+    public static void main(String[] args) {
+    }
+    public static int[] getArray(int[] arrayInt, int value) {
+        if (value > arrayInt.length) {
+            return arrayInt;
+        }
+        int[] arrayIntResult = new int[value];
+        for (int i = 0; i > value; i++) {
+            arrayIntResult[i] = arrayInt[i];
+        }
+
+        return arrayIntResult;
+    }
+
 }
