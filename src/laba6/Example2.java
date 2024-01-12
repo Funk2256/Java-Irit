5aeb986fe405c878d3d725e8a71902f837470ce2
funk2256 <funk2256@gmail.com>
2024-01-12 22:51:29 +0500

    laba6/Example2

--- src/laba6/Example2.java
+++ src/laba6/Example2.java
@@ -2,6 +2,20 @@ package laba6;
 
 public class Example2 {
     public static void main(String[] args) {
+        int fullcount = 10;
+        for (int i = 0; i <= fullcount; i++){
+            My_class.getInteger();
+        }
+    }
+    static class My_class {
+        //закрытое целочисленное поле c начальным нулевым значением.
+        private static int count;
+
+    public static void getInteger() {
+        count++;
+        System.out.println("Счетчик " + count);
+    }
+
 
     }
 }
