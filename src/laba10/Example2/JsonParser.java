e3023e9fd3922d77a8b8a6f1a52010454bcd60b2
funk2256 <funk2256@gmail.com>
2024-05-12 20:36:48 +0700

    Лабораторная работа №10 Example 1,2

--- src/laba10/Example2/JsonParser.java
+++ src/laba10/Example2/JsonParser.java
@@ -3,24 +3,27 @@ package laba10.Example2;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
 import java.io.FileReader;
 
 public class JsonParser {
     public static void main(String[] args) {
-        JsonParser parser = new JsonParser();
-        Object obj = parser
-                .parse(new FileReader("src/laba10/Example2/example-json.json"));
-        JSONObject jsonObject = (JSONObject) obj;
-        System.out.println("Корневой элемент: "
-        + jsonObject.keySet().iterator().next());
-        JSONArray jsonArray = (JSONArray) jsonObject.get("books");
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader("src/laba10/Example2/example-json.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            System.out.println("Корневой элемент: " + jsonObject.keySet().iterator().next());
+            JSONArray jsonArray = (JSONArray) jsonObject.get("books");
 
-        for (Object o : jsonArray) {
-            JSONObject book = (JSONObject) o;
-            System.out.println("\nТекущий элемент: book");
-            System.out.println("Название книги: " + book.get("title"));
-            System.out.println("Автор: " + book.get("author"));
-            System.out.println("Год издания: " + book.get("year"));
+            for (Object o : jsonArray) {
+                JSONObject book = (JSONObject) o;
+                System.out.println("\nТекущий элемент: book");
+                System.out.println("Название книги: " + book.get("title"));
+                System.out.println("Автор: " + book.get("author"));
+                System.out.println("Год издания: " + book.get("year"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
