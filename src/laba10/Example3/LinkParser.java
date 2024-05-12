bab4aa94e6b46c6af7d148ce1865a80bed333ec2
funk2256 <funk2256@gmail.com>
2024-05-12 21:04:36 +0700

    Лабораторная работа №10 Example 3

---
+++ src/laba10/Example3/LinkParser.java
@@ -0,0 +1,24 @@
+package laba10.Example3;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
+public class LinkParser {
+    public static void main(String[] args) {
+        String url = "https://itlearn.ru/first-steps";
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+                System.out.println(link.attr("abs:href"));
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
